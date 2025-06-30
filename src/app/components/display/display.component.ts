72bb87919951f57afe20bb3026074234c240106c
Aayush Sharma <aayush.sharma.as95@gmail.com>
2025-06-30 19:58:45 +0530

    Display Data

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './display.component.scss'
 })
 export class DisplayComponent implements OnInit {
+  countries: any = {};
 constructor(private dataService: DataService) {}
 
 ngOnInit() {
   this.dataService.getCountries().subscribe((res) => {
-    
+    this.countries = Object.entries(res.data).map(([code, details]: any) => ({
+        code,
+        ...details,
+      }));
   })
 }
 }
